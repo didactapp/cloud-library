8b9ebf3f0f883fbfeed6511da8f658395f3942b5
Roman Smirnov <roman.smirnv@gmail.com>
2018-03-07 03:20:08 +0200

    ˆ
    Signed-off-by: Roman Smirnov <roman.smirnv@gmail.com>

---
+++ app/src/main/java/com/didactapp/android/cloudlibrary/network/ApiInterface.java
@@ -0,0 +1,8 @@
+//package com.didactapp.android.cloudlibrary.network;
+//
+//public interface ApiInterface {
+//
+//
+//    @GET("movie/popular")
+//    Call<MoviesHolder> getPopularMovies(@Query("api_key") String apiKey);
+//}
